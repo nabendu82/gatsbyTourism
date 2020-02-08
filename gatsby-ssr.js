96489cb4cd34df5e6191435913dd0f9658a9d917
nabendu82 <nabendu.biswas@gmail.com>
2020-02-08 22:04:22 +0530

    Ad unit changed

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
 const React = require("react");
 
-exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents, setPreBodyComponents }) {
+exports.onRenderBody = function({ setHeadComponents, setPreBodyComponents }) {
 
     setHeadComponents([
         <script
@@ -16,13 +16,13 @@ exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents, setP
     ]);
 
     setPreBodyComponents([
-            <div style={{maxWidth: '54vw', margin: '0 auto'}} id="356636763">
+            <div id="255758264">
                 <script
                     dangerouslySetInnerHTML={{
                         __html:`
                         try {
                             window._mNHandle.queue.push(function (){
-                                window._mNDetails.loadTag("356636763", "728x90", "356636763");
+                                window._mNDetails.loadTag("255758264", "728x90", "255758264");
                             });
                         }
                         catch (error) {}
@@ -32,37 +32,5 @@ exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents, setP
             </div>
     ]);
 
-    setPostBodyComponents([
-        <React.Fragment>
-            <div style={{display: 'flex', alignItem: 'center', justifyContent: 'center' }} id="104240845">
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html:`
-                        try {
-                            window._mNHandle.queue.push(function (){
-                                window._mNDetails.loadTag("104240845", "300x250", "104240845");
-                            });
-                        }
-                        catch (error) {}
-                        `
-                    }}
-                />
-            </div>
-            <div id="172542266">
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html:`
-                        try {
-                            window._mNHandle.queue.push(function (){
-                                window._mNDetails.loadTag("172542266", "320x50", "172542266");
-                            });
-                        }
-                        catch (error) {}
-                        `
-                    }}
-                />
-            </div>
-        </React.Fragment>
-    ]);
 
 };
